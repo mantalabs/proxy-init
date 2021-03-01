097f917f1e49c7a803b2becbebc4fb1e0bc0b281
Silas Boyd-Wickizer <silasbw@gmail.com>
2021-02-28 20:11:41 -0800

    chore: some initial skeleton code (#1)

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/mantalabs/proxy-init
+
+go 1.15
+
+require (
+	github.com/blang/semver v3.5.1+incompatible // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/spec v0.20.3 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/howeyc/gopass v0.0.0-20190910152052-7cb4b85ec19c // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/juju/ratelimit v1.0.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pborman/uuid v1.2.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/ugorji/go v1.2.4 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	k8s.io/client-go v0.20.0
+	k8s.io/klog/v2 v2.5.0
+)
---
+++ main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/exec"
+	
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"k8s.io/klog/v2"
+)
+
+//
+// TODO(sbw): change to "bootnode" and "geth" when ready to deploy.
+//
+var bootnodeFile = "./bin/bootnode"
+var gethFile = "./bin/geth"
+
+func main() {
+	klog.InitFlags(nil)
+
+	var kubeconfig string
+	var privateKeyPath string
+	var keystorePath string
+	var passwordPath string
+	var internalAddress string
+	var externalAddress string
+	
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "absolute path to the kubeconfig file")
+	flag.StringVar(&privateKeyPath, "private-key", "", "path to write private key to")
+	flag.StringVar(&keystorePath, "keystore", "", "path to keystore")
+	flag.StringVar(&passwordPath, "password", "", "path to write password file to")
+	flag.StringVar(&internalAddress, "internal-address", "", "internal proxy address")
+	flag.StringVar(&externalAddress, "external-address", "", "external proxy address")
+	flag.Parse()
+
+	if privateKeyPath == "" {
+		klog.Fatalf("-private-key required")
+	}
+	if keystorePath == "" {
+		klog.Fatalf("-keystore required")
+	}
+	if passwordPath == "" {
+		klog.Fatalf("-password required")
+	}
+	if internalAddress == "" {
+		klog.Fatalf("-internal-address required")
+	}
+	if externalAddress == "" {
+		klog.Fatalf("-external-address required")
+	}
+
+	bootnodeExecPath, err := exec.LookPath(bootnodeFile)
+	if err != nil {
+		klog.Fatalf("Failed to find bootnode: %v", err)
+	}
+	gethExecPath, err := exec.LookPath(gethFile)
+	if err != nil {
+		klog.Fatalf("Failed to find geth: %v", err)
+	}
+
+	//
+	// Generate a new throw-away key that we plan on passing to the Proxy.
+	//
+	cmdGenkey := exec.Cmd{
+		Path: bootnodeExecPath,
+		Args: []string{bootnodeExecPath, "-genkey", privateKeyPath},
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+	if err := cmdGenkey.Run(); err != nil {
+		klog.Fatalf("'bootnode -genkey' failed: %v", err)
+	}
+	klog.Infof("Generated private key %s", privateKeyPath)	
+
+	//
+	// TODO(sbw): use bootnode to write the *public key* to a file
+	//
+	publicKey := ""
+
+	//
+	// TODO(sbw): generate a password file with random password and write to passwordPath
+	//
+	password := ""
+
+	gethImportCmd := exec.Cmd{
+		Path: gethExecPath,
+		Args: []string{
+			gethExecPath,
+			"account", "import",
+			"--keystore", keystorePath,
+			"--password", passwordPath,
+			privateKeyPath,
+		},
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+	if err := gethImportCmd.Run(); err != nil {
+		klog.Fatalf("'geth account import' failed: %v", err)
+	}
+	klog.Infof("Imported private key %s", privateKeyPath)	
+
+	//
+	// TODO(sbw): extract *account address* from the filename in the keystore
+	// or the JSON blob in the file contents.
+	//
+	accountAddress := ""
+	accountAddressPath := ""
+
+	//
+	// TODO(sbw): publish enodes (e.g., to a ConfigMap) so the proxy-informer
+	// will discover them.
+	//
+	// Given the private key file and IP, bootnode will generate the enode.
+	//
+	internalEnode := ""
+	externalEnode := ""
+
+	//
+	// Create kubernetes client
+	//
+	if false {
+		clientset, err := newClientset(kubeconfig)
+		if err != nil {
+			klog.Fatalf("Failed to connect to cluster: %v", err)
+		}
+
+		fmt.Printf("clientset=%v\n", clientset)
+
+		publishEnodes(clientset, internalEnode, externalEnode)
+	}
+}
+
+func publishEnodes(clientset *kubernetes.Clientset, internalEnode string, externalEnode string) (*kubernetes.Clientset, error) {
+	return nil, nil
+}
+
+func newClientset(filename string) (*kubernetes.Clientset, error) {
+	config, err := getConfig(filename)
+	if err != nil {
+		return nil, err
+	}
+	return kubernetes.NewForConfig(config)
+}
+
+func getConfig(cfg string) (*rest.Config, error) {
+	if cfg == "" {
+		return rest.InClusterConfig()
+	}
+	return clientcmd.BuildConfigFromFlags("", cfg)
+}
