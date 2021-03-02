b483dad74570b6abd8ae4ecf52facae71f955b87
Silas Boyd-Wickizer <silasbw@gmail.com>
2021-03-01 18:24:17 -0800

    Key generation etc. (#7)

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -42,6 +45,7 @@ func main() {
 	var privateKeyPath string
 	var keystorePath string
 	var passwordPath string
+	var accountAddressPath string
 	var internalAddress string
 	var externalAddress string
 
@@ -49,6 +53,7 @@ func main() {
 	flag.StringVar(&privateKeyPath, "private-key", "", "path to write private key to")
 	flag.StringVar(&keystorePath, "keystore", "", "path to keystore")
 	flag.StringVar(&passwordPath, "password", "", "path to write password file to")
+	flag.StringVar(&accountAddressPath, "account-address", "", "path to write account address to")
 	flag.StringVar(&internalAddress, "internal-address", "", "internal proxy address")
 	flag.StringVar(&externalAddress, "external-address", "", "external proxy address")
 	flag.Parse()
@@ -61,6 +66,9 @@ func main() {
 	}
 	if passwordPath == "" {
 		klog.Fatalf("-password required")
+	}
+	if accountAddressPath == "" {
+		klog.Fatalf("-account-address required")
 	}
 	if internalAddress == "" {
 		klog.Fatalf("-internal-address required")
@@ -112,14 +120,10 @@ func main() {
 	// Generate a password file with random password and write to passwordPath
 	//
 	password := genPass()
-	passwordFile, err := os.Create(passwordPath)
+	err = ioutil.WriteFile(passwordPath, []byte(password), 0600)
 	if err != nil {
 		klog.Fatalf("Unable to create password file: %v", err)
 	}
-	if _, err := passwordFile.WriteString(password); err != nil {
-		klog.Fatalf("Unable to write password file: %v", err)
-	}
-	return
 
 	gethImportCmd := exec.Cmd{
 		Path: gethExecPath,
@@ -138,13 +142,45 @@ func main() {
 	}
 	klog.Infof("Imported private key %s", privateKeyPath)
 
-	//
-	// TODO(sbw): extract *account address* from the filename in the keystore
+	// Extract *account address* from the filename in the keystore
 	// or the JSON blob in the file contents.
 	//
-	accountAddress := ""
-	accountAddressPath := ""
-	klog.Infof("Extracted account address %s from %s", accountAddress, accountAddressPath)
+	// (1) Find the keystore JSON file. The keystore file has a name like:
+	//
+	//   keystore-path/UTC--2021-03-01T05-17-12.173336000Z--2754599e48ca29f1998c31e7c668c33bff5e5bf2
+	//
+	keystoreJsonPath := ""
+	matchCount := 0
+	matches, err := filepath.Glob(keystorePath + "/*")
+	if err != nil {
+		klog.Fatalf("Couldn't expand glob: %s/*: %v", keystorePath, err)
+	}
+	for _, match := range matches {
+		keystoreJsonPath = match
+		matchCount += 1
+	}
+	if matchCount != 1 {
+		klog.Fatalf("Expected exactly 1 file in keystore; got: %d", matchCount)
+	}
+	// (2) Load the JSON blob to extract the address.
+	keystoreJsonContent, err := ioutil.ReadFile(keystoreJsonPath)
+	if err != nil {
+		klog.Fatalf("Couldn't read keystore file %s: %v", keystoreJsonPath, err)
+	}
+	var keystore map[string]interface{}
+	err = json.Unmarshal([]byte(keystoreJsonContent), &keystore)
+	if err != nil {
+		klog.Fatalf("Couldn't unmarshal keystore data: %v", err)
+	}
+	accountAddress := keystore["address"]
+	klog.Infof("Extracted account address from keytore: %s", accountAddress)
+	// (3) Write the address to he requested path
+	err = ioutil.WriteFile(accountAddressPath, []byte(accountAddress.(string)), 0644)
+	if err != nil {
+		klog.Fatalf("Couldn't write account address to %s: %v", accountAddressPath, err)
+	}
+	klog.Infof("Wrote account address to %s", accountAddressPath)
+	return
 
 	//
 	// TODO(sbw): publish enodes (e.g., to a ConfigMap) so the proxy-informer
